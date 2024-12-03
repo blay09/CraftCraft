713a90dde62927ff8f2987d4b57e7ff675474bc7
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-12-03 20:57:04 +0100

    feat: Update to Minecraft 1.21.4

--- fabric/src/main/java/net/blay09/mods/craftingslots/fabric/datagen/ModModelProvider.java
+++ fabric/src/main/java/net/blay09/mods/craftingslots/fabric/datagen/ModModelProvider.java
@@ -1,11 +1,11 @@
 package net.blay09.mods.craftingslots.fabric.datagen;
 
 import net.blay09.mods.craftingslots.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.models.BlockModelGenerators;
-import net.minecraft.data.models.ItemModelGenerators;
-import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
