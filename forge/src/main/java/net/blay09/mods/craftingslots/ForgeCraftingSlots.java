27a9330e6e96fdcf14dc11022fcb9b5fcb201475
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:28:04 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/craftingslots/ForgeCraftingSlots.java
+++ forge/src/main/java/net/blay09/mods/craftingslots/ForgeCraftingSlots.java
@@ -1,17 +1,20 @@
 package net.blay09.mods.craftingslots;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.craftingslots.client.CraftingSlotsClient;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(CraftingSlots.MOD_ID)
 public class ForgeCraftingSlots {
-    public ForgeCraftingSlots() {
-        Balm.initialize(CraftingSlots.MOD_ID, EmptyLoadContext.INSTANCE, CraftingSlots::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(CraftingSlots.MOD_ID, EmptyLoadContext.INSTANCE, CraftingSlotsClient::initialize));
+    public ForgeCraftingSlots(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(CraftingSlots.MOD_ID, loadContext, CraftingSlots::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(CraftingSlots.MOD_ID, loadContext, CraftingSlotsClient::initialize);
+        }
     }
 }
